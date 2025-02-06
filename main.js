ca140f7d9498cf5f9cc3df680282fbb98e5c7548
25JT <jhonierlucumimeza@gmial.com>
2025-02-06 12:15:23 -0500

    initial comit

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-document.getElementById("Name").innerHTML = "MOBILE";
-document.getElementById("last").innerHTML = "REPAIR";
+
+    document.getElementById('repar').innerHTML = 'REPARACION DE MOBILE PROFESIONAL'
+
+
+
+
+var typed = new Typed('#Name', {
+    strings: ["BIENVENIDOS", "MOBILE"],
+    typeSpeed: 40,
+    showCursor: false,
+    loop: false,
+  });
+
+  var typed = new Typed('#last', {
+    strings: [" ","A", "REPAIR"],
+    typeSpeed: 100,
+    showCursor: false,
+  });
+  
