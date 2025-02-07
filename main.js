ff74f71fe246d1a200748bddfb6fedc734599feb
25JT <jhonierlucumimeza@gmial.com>
2025-02-07 14:36:24 -0500

    Animacion mobile repair y descripcion de la pagina

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
     document.getElementById('repar').innerHTML = 'REPARACION DE MOBILE PROFESIONAL'
-
-
+    document.getElementById('tPresentacion').innerHTML = '¡TU MÓVIL EN LAS MEJORES MANOS!'
+    document.getElementById('descripcion').innerHTML = 'En [Nombre de tu empresa], sabemos lo importante que es tu móvil para ti. Ya sea que se haya caído, mojado o simplemente haya dejado de funcionar, estamos aquí para devolvértelo como nuevo.'
 
 
 var typed = new Typed('#Name', {
