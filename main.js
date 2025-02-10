9d90fa2d81e548c333ea1e8b5906cc6f98025a93
25JT <jhonierlucumimeza@gmial.com>
2025-02-10 15:01:16 -0500

    INICIO PARTADO CONTACTANOS

--- main.js
+++ main.js
@@ -3,8 +3,9 @@
     document.getElementById('tPresentacion').innerHTML = '¡TU MÓVIL EN LAS MEJORES MANOS!'
     document.getElementById('descripcion').innerHTML = 'En [Nombre de tu empresa], sabemos lo importante que es tu móvil para ti. Ya sea que se haya caído, mojado o simplemente haya dejado de funcionar, estamos aquí para devolvértelo como nuevo.'
     document.getElementById('servicios').innerHTML = 'NUESTROS SERVICIOS'
-
-
+    document.getElementById('confia').innerHTML = 'CONFÍA EN LOS EXPERTOS'
+    document.getElementById('segundaVida').innerHTML = 'DALE UNA SEGUNDA VIDA A TU SMARTPHONE'
+    document.getElementById('nosotros').innerHTML = 'CON NOSOTROS'
 var typed = new Typed('#Name', {
     strings: ["BIENVENIDOS", "MOBILE"],
     typeSpeed: 40,
