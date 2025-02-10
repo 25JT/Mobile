b287d196322076a9c08e19bcc48a8a804cba39f9
25JT <jhonierlucumimeza@gmial.com>
2025-02-10 10:44:41 -0500

    Reparacion boli imagen y animacion empiezo  nuestros servicios

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
     document.getElementById('repar').innerHTML = 'REPARACION DE MOBILE PROFESIONAL'
     document.getElementById('tPresentacion').innerHTML = '¡TU MÓVIL EN LAS MEJORES MANOS!'
     document.getElementById('descripcion').innerHTML = 'En [Nombre de tu empresa], sabemos lo importante que es tu móvil para ti. Ya sea que se haya caído, mojado o simplemente haya dejado de funcionar, estamos aquí para devolvértelo como nuevo.'
+    document.getElementById('servicios').innerHTML = 'NUESTROS SERVICIOS'
 
 
 var typed = new Typed('#Name', {
